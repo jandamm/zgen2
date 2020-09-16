9308eacd16c285ad38a40657be13852df34e4725
Jan Dammshäuser <mail@jandamm.de>
2020-09-16 23:22:16 +0200

    Add full support for lazy loading zgen

--- zgen-lazy.zsh
+++ zgen-lazy.zsh
@@ -1,3 +1,37 @@
-# TODO Add real lazy loading
+#!/bin/zsh
+# vim: set ft=zsh fenc=utf-8 noai ts=4 et sts=4 sw=0 tw=80 nowrap :
 local ZGEN_SOURCE="$0:A:h"
-source "${ZGEN_SOURCE}/zgen.zsh"
+
+if [[ -z "${ZGEN_DIR}" ]]; then
+    ZGEN_DIR="${HOME}/.zgen"
+fi
+
+if [[ -z "${ZGEN_INIT}" ]]; then
+    ZGEN_INIT="${ZGEN_DIR}/init.zsh"
+fi
+
+zgen-saved() {
+    [[ -f "${ZGEN_INIT}" ]] && return 0 || return 1
+}
+
+zgen-init() {
+    if [[ -f "${ZGEN_INIT}" ]]; then
+        source "${ZGEN_INIT}"
+    fi
+}
+
+# Run initialization only once
+if [[ -z $ZGEN_LAZY_LOCK ]]; then
+    zgen() {
+        if [[ $1 = 'saved' ]]; then
+            zgen-saved
+        else
+            ZGEN_LAZY_LOCK=yes
+            source "${ZGEN_SOURCE}/zgen.zsh"
+            unset ZGEN_LAZY_LOCK
+            zgen $@
+        fi
+    }
+    fpath=($ZGEN_SOURCE $fpath)
+    zgen-init
+fi
